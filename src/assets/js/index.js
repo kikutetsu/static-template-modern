87718407ae1be6db0679d145145a4f4f16c1c0e2
kikuchi <kikuchi.3989@icloud.com>
2018-12-26 00:52:41 +0900

    add babel

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,4 +3,10 @@ import '../style/style.scss';
 window.onload = function() {
   const body = document.querySelector('body');
   body.innerHTML += 'Hello World!';
+  test();
+};
+
+const test = () => {
+  let hello = 'Hello babel!';
+  console.log(hello);
 };
